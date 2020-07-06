19496151cbc030297888c06bb336131254f6a70e
Eli Block <eliyahu.block@gmail.com>
2020-07-06 13:31:39 -0400

    Sign in out links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  authenticated :user do
+    root to: "home#index", as: :authenticated_root
+  end
+
+  unauthenticated :user do
+    root to: "pages#landing", as: :unauthenticated_root
+  end
 end
